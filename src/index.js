6a5060b1ad5f66ca3a1e6077afe4ff77e50ad8c6
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-28 11:48:23 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    
+    e.preventDefault();
+    buildTaskList(e.target.new-task-description.value);
+    form.reset();
+  })
 });
+
+function buildTaskList(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  li.textContent = `${todo} `;
+  
+  let ul = document.getElementById('tasks');
+  ul.appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
